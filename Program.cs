5683cb72e7dd43f44b5db8f8f8a9719d44507f33
Alexandr <jwchaganav@gmail.com>
2022-11-22 11:37:06 +0400

    sortArray

--- Program.cs
+++ Program.cs
@@ -7,8 +7,50 @@ int minRnd = 1; int maxRnd = 10; // для random
 //
 FillRandomArray(numbers);
 Print2DArray(numbers);
+Sort2DArray(numbers);
+Console.WriteLine();
+Print2DArray(numbers);
 
 // Функции
+void SortArray(int[] array)
+{
+    int max = array[0];
+    int temp = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        for(int j = 1; j < array.Length; j++)
+        {
+            if(array[i] < array[j])
+            {
+                temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+            
+        }
+    }
+}
+void Sort2DArray(int[,] array)
+{
+    int temp = 0;
+    for(int row = 0; row < array.GetLength(0); row++)
+    {
+        for(int col = 0; col < array.GetLength(1); col++)
+        {
+            if(col < array.GetLength(1)-1)
+            {
+                temp = array[row,col];
+                if(array[row,col] < array[row,col+1])
+                {
+                    array[row,col] = array[row,col+1];
+                    array[row,col+1] = temp;
+                }
+                
+            }
+            
+        }
+    }
+}
 void FillRandomArray(int[,] array)
 {
     Random rnd = new Random();
