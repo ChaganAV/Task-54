896acdddfb5fffc87dedaaa5a142e0309f9e2277
Alexandr <jwchaganav@gmail.com>
2022-11-22 12:10:44 +0400

    тест прошел

--- Program.cs
+++ Program.cs
@@ -4,51 +4,58 @@ int minR = 2; int maxR = 10;
 int rows = rnd.Next(minR,maxR); int columns = rnd.Next(minR,maxR); // строки и колонки массива 
 int[,] numbers = new int[rows,columns]; // массив
 int minRnd = 1; int maxRnd = 10; // для random
+int[] test = {1,4,3,6};
 //
 FillRandomArray(numbers);
 Print2DArray(numbers);
 Sort2DArray(numbers);
 Console.WriteLine();
 Print2DArray(numbers);
+//SortArrayTest(test);
+//PrintArray(test);
 
 // Функции
-void SortArray(int[] array)
+void SortArrayTest(int[] array)
 {
-    int max = array[0];
     int temp = 0;
     for(int i = 0; i < array.Length; i++)
     {
-        for(int j = 1; j < array.Length; j++)
+        for(int j = i+1; j < array.Length; j++)
         {
+            Console.WriteLine($"array[i] < array[j] {array[i]} < {array[j]}");
             if(array[i] < array[j])
             {
                 temp = array[i];
                 array[i] = array[j];
                 array[j] = temp;
             }
+        }
+        Console.WriteLine($"Проход {i}");
+    }
+}
+void SortArray(int row, int[,] array)
+{
+    int temp = 0;
+    for(int i = 0; i < array.GetLength(1); i++)
+    {
+        for(int j = i+1; j < array.GetLength(1); j++)
+        {
+            if(array[row,i] < array[row,j])
+            {
+                temp = array[row,i];
+                array[row,i] = array[row,j];
+                array[row,j] = temp;
+            }
             
         }
     }
 }
-void Sort2DArray(int[,] array)
+void Sort2DArray(int[,] array2D)
 {
     int temp = 0;
-    for(int row = 0; row < array.GetLength(0); row++)
+    for(int row = 0; row < array2D.GetLength(0); row++)
     {
-        for(int col = 0; col < array.GetLength(1); col++)
-        {
-            if(col < array.GetLength(1)-1)
-            {
-                temp = array[row,col];
-                if(array[row,col] < array[row,col+1])
-                {
-                    array[row,col] = array[row,col+1];
-                    array[row,col+1] = temp;
-                }
-                
-            }
-            
-        }
+        SortArray(row, array2D);
     }
 }
 void FillRandomArray(int[,] array)
@@ -73,4 +80,12 @@ void Print2DArray(int[,] array)
         }
         Console.WriteLine();
     }
+}
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
 }
